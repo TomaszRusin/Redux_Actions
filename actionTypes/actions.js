4ff9ad4620ebf9631838e28c91641f56253835ef
Tomasz Rusin <tomek@rusin.info>
2019-06-20 22:07:38 +0200

    Repaired code

--- actionTypes/actions.js
+++ actionTypes/actions.js
@@ -1,10 +1,15 @@
 
-// ADD_COMMENT
-
-export const ADD_COMMENT = 'ADD_COMMENT'
 import uuid from 'uuid'
 
-function addComment(text) {
+export const ADD_COMMENT = 'ADD_COMMENT'
+export const EDIT_COMMENT = 'EDIT_COMMENT'
+export const REMOVE_COMMENT = 'REMOVE_COMMENT'
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT'
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT'
+
+// ADD_COMMENT
+
+export function addComment(text) {
     return {
         type: ADD_COMMENT,
         text,
@@ -12,13 +17,9 @@ function addComment(text) {
     }
 }
 
-export const boundAddComment = text => dispatch(addComment(text));
-
 // EDIT_COMMENT
 
-export const EDIT_COMMENT = 'EDIT_COMMENT'
-
-function editComment(text, id) {
+export function editComment(id, text) {
     return {
         type: EDIT_COMMENT,
         text,
@@ -26,45 +27,30 @@ function editComment(text, id) {
     }
 }
 
-export const boundEditComment = text => dispatch(editComment(text));
-
 // REMOVE_COMMENT
 
-export const REMOVE_COMMENT = 'REMOVE_COMMENT'
-
-function removeComment(id) {
+export function removeComment(id) {
     return {
         type: REMOVE_COMMENT,
         id: id
     }
 }
 
-export const boundRemoveComment = text => dispatch(removeComment(text));
-
 // THUMB_DOWN_COMMENT
 
-export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT'
-
-function downComment(id) {
+export function downComment(id) {
     return {
         type: THUMB_DOWN_COMMENT,
-        id: id,
-        votes: votes - 1
+        id: id
     }
 }
-
-export const boundDownComment = text => dispatch(downComment(text));
 
 // THUMB_UP_COMMENT
 
-export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT'
-
-function upComment(id) {
+export function upComment(id) {
     return {
         type: THUMB_UP_COMMENT,
-        id: id,
-        votes: votes + 1
+        id: id
     }
 }
-
-export const boundUpComment = text => dispatch(upComment(text));
+ 
